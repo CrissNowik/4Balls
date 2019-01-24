26b502ad8cc5d31ac05b2bb60b385500e798ca95
Krzysztof Nowicki <krzysztof11nowicki@gmail.com>
2019-01-24 22:19:13 +0100

    html structure for instruction and players

--- js/Website.js
+++ js/Website.js
@@ -1,3 +1,5 @@
+import { ftruncate } from "fs";
+
 /**
  * SS - ScreenSwitcher - object used for switching pages of the app
  */
@@ -7,25 +9,45 @@ const SS = {
     screen3: document.getElementById("instruction"),
     screen4: document.getElementById("players"),
     screen5: document.getElementById("game"),
+    btn1: document.getElementById("goToPlayers"),
+    btn2: document.getElementById("goToGame"),
+    draw1: document.getElementById("goToDraw2"),
+    draw2: document.getElementById("goToDraw3"),
+    draw3: document.getElementById("goToDraw4"),
+    btnPrev: document.getElementById("prevDraw"),
+    btnNext: document.getElementById("nextDraw"),
+
 // method used to switching pages of the game
     _goNext: function(toHide, toShow) {
         toHide.style.display = "none";
         toShow.style.display = "flex";
         console.log();
+    },
+    _switchVisibility: function(){
         
     }
 }
+
 
 SS.screen2.addEventListener("click", function(e){
     e.preventDefault();
     SS._goNext(SS.screen2, SS.screen3);
 });
-SS.screen3.addEventListener("click", function(e){
+SS.btn1.addEventListener("click", function(e){
     e.preventDefault();
     SS._goNext(SS.screen3, SS.screen4);
 });
-SS.screen4.addEventListener("click", function(e){
+SS.btn2.addEventListener("click", function(e){
     e.preventDefault();
     SS._goNext(SS.screen1, SS.screen5);
 });
 
+
+SS.btnPrev.addEventListener("click", function(e){
+    e.preventDefault();
+    SS._goNext(SS.screen1, SS.screen5);
+});
+SS.btnNext.addEventListener("click", function(e){
+    e.preventDefault();
+    SS._goNext(SS.screen1, SS.screen5);
+});
