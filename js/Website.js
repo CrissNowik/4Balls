8095acae2a42f2cf952610d89d1d7a3e76833917
Krzysztof Nowicki <krzysztof11nowicki@gmail.com>
2019-01-25 23:59:34 +0100

    mainScreenPlayers scss

--- js/Website.js
+++ js/Website.js
@@ -7,13 +7,10 @@ const SS = {
     screen3: document.getElementById("instruction"),
     screen4: document.getElementById("players"),
     screen5: document.getElementById("game"),
-    btn1: document.getElementById("goToPlayers"),
-    btn2: document.getElementById("goToGame"),
-    draw1: document.getElementById("goToDraw2"),
-    draw2: document.getElementById("goToDraw3"),
-    draw3: document.getElementById("goToDraw4"),
     btnPrev: document.getElementById("prevDraw"),
     btnNext: document.getElementById("nextDraw"),
+    btnSkip: document.getElementById("skip"),
+    btn1: document.getElementById("goToGame"),
 
 // method used to switching pages of the game
     _goNext: function(toHide, toShow) {
@@ -26,26 +23,17 @@ const SS = {
     }
 }
 
-
+// main screen switches
 SS.screen2.addEventListener("click", function(e){
     e.preventDefault();
     SS._goNext(SS.screen2, SS.screen3);
 });
-SS.btn1.addEventListener("click", function(e){
+SS.btnSkip.addEventListener("click", function(e){
     e.preventDefault();
     SS._goNext(SS.screen3, SS.screen4);
 });
-SS.btn2.addEventListener("click", function(e){
+SS.btn1.addEventListener("click", function(e){
     e.preventDefault();
     SS._goNext(SS.screen1, SS.screen5);
 });
 
-
-SS.btnPrev.addEventListener("click", function(e){
-    e.preventDefault();
-    SS._goNext(SS.screen1, SS.screen5);
-});
-SS.btnNext.addEventListener("click", function(e){
-    e.preventDefault();
-    SS._goNext(SS.screen1, SS.screen5);
-});
